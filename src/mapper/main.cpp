b82f26e0eded1d1fa72a1c08d04d9da3bbfdcb02
future <sprnv4777@gmail.com>
2025-02-12 19:38:17 +0500

    we switch to cs2 now

--- src/mapper/main.cpp
+++ src/mapper/main.cpp
@@ -9,7 +9,7 @@ int main( const int argc, char *argv[] ) {
         return EXIT_FAILURE;
     }
 
-    const map::loader< map::variant::LOADLIBRARY > loader{ L"tf_win64.exe" };
+    const map::loader< map::variant::LOADLIBRARY > loader{ L"cs2.exe" };
 
     if ( loader.inject( argv[ 1 ] ) )
         std::cout << "[+] injected successfully\n";
